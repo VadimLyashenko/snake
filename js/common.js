d69758c77e37d09752dd35eb56e606636cedadca
Worst <terateratera23@gmail.com>
2017-11-02 16:48:36 +1000

    add images

--- js/common.js
+++ js/common.js
@@ -1,12 +1,13 @@
 //инициализация переменных	
 function init(){
-		//объект - game - игровое поле
-	game = new rect('#fff', 0, 0, 800, 600);
+	//объект - game - игровое поле
+	//game = new rect('#fff', 0, 0, 800, 600);
   key_delay = true;
+  tail_side = true;
 	//--------------------
 	canvas = document.getElementById('canvas_id');
-	canvas.width = game.width;
-	canvas.height = game.height;
+	canvas.width = 800;
+	canvas.height = 600
 	context = canvas.getContext('2d'); 
 	//--------------------
 	
@@ -14,36 +15,34 @@ function init(){
 	step = 50;
 	//первоначальное направление движения(1 вверх, 2 вправо, 3 вниз, 0 влево)
 	check = 2;	
-		//скорость
+	//начальная скорость
   speed = 7;
 
 	//голова
 	side_head = step;
 	head_posx = 150;
 	head_posy = 150;
-	head = new rect('#000', head_posx,head_posy, side_head, side_head);
+	head = new imag(head_posx,head_posy);
 	
 	//хвост
-	tail_quan = 3; //первоначальное кол-во клеток хвоста
-	tail_color = '#888'; //цвет хвоста
+	tail_quan = 4; //первоначальное кол-во клеток хвоста
 	tail = [];
-	createTail(tail_color, tail_quan);
+	createTail(tail_quan);
 	
 	//еда
-	eat = new rect('#3e3', 100, 100, side_head, side_head);
+	eat = new imag(0, 0);
 	checkCoordEat();
-	game.drawRect();
  
+	//картинка фона
+	backgr = new imag(0, 0);
+
   //цикл
 	timer = setTimeout(play, 1000/speed);
 }
 
 //отрисовка
 function draw(){
-	game.clRect(); 
-	head.drawRect();
-	drawTail();
-	eat.drawRect();
+	
 }
 
 //главная функция цикла
@@ -52,35 +51,59 @@ function play(){
 	checkEat();
 	var death = checkDeath();
 	if(!death){
+		backgr.drawImg(backgr_pic);
+		drawHead();
 		updateTail();
-		draw();
+		drawTail();
+		eat.drawImg(food_pic);
 	}
 	timer = setTimeout(play, 1000/speed);
 	if(death){
 		clearTimeout(timer);
-
 	}
 }
 
-
+//обновление координат головы
 function updateHead(){
 	switch (check) {
 		case 0:
-				head.x = head.x - step;
+				head.x = head.x - step;	
 			break;
 		case 1:
-				head.y = head.y - step;
+				head.y = head.y - step;		
 			break;
 		case 2:
-				head.x = head.x + step;			
+				head.x = head.x + step;							
 			break;
 		case 3:
-				head.y = head.y + step;	
+				head.y = head.y + step;			
 			break;
 		default:
 			break;
 	}
 }
+
+//присваивание картинки для головы в зависимости от направления и отрисовка
+function drawHead(){
+	switch (check) {
+		case 0:
+				head.drawImg(head_left_pic);
+			break;
+		case 1:
+				head.drawImg(head_top_pic);
+			break;
+		case 2:
+				head.drawImg(head_right_pic);
+			break;
+		case 3:
+				head.drawImg(head_bot_pic);
+			break;
+		default:
+			break;
+	}
+}
+
+
 
 function checkDeath(){
 	//проверка левой стороны
@@ -92,11 +115,11 @@ function checkDeath(){
 		return true;
 	}
 	//проверка правой стороны
-	if(head.x > game.width - step){
+	if(head.x > canvas.width - step){
 		return true;
 	}
 	//проверка нижней стороны
-	if(head.y > game.height - step){
+	if(head.y > canvas.height - step){
 		return true;
 	}
 	//проверка налезания на хвост
@@ -121,8 +144,8 @@ function checkEat(){
 
 //вычисление коорожинат для еды
 function checkCoordEat(){
-	eat.x = getRandom(0, game.width - step,step);
-	eat.y = getRandom(0, game.height - step,step);
+	eat.x = getRandom(0, canvas.width - step,step);
+	eat.y = getRandom(0, canvas.height - step,step);
 	var b = true;	
 	for(var i = 0; i < tail.length; i++){
 		if((eat.x == tail[i].x) && (eat.y == tail[i].y)){
@@ -147,8 +170,6 @@ function getRandom(min, max, step){
 	}
 	return a;
 }
-
-
 
 
 //влево - 0, вверх - 1, вправо - 2, вниз - 3 
@@ -188,25 +209,206 @@ document.body.onkeydown = function(e){
 
 }
 
-//функция-коструктор для прямоугольников
-function rect(color, x, y, width, height) {
-    this.color = color; // цвет прямоугольника
-    this.x = x; // координата х
+//функция-коструктор для картинок
+function imag(x, y) {
+    this.x = x; // координата х на холсте
     this.y = y; // координата у
-    this.width = width; // ширина
-    this.height = height; // высота
-    this.drawRect = function() // Метод рисующий прямоугольник
+    this.drawImg = function(pic) // Метод рисующий картинку
     {
-        context.fillStyle = this.color;
-        context.fillRect(this.x, this.y, this.width, this.height);
+				context.drawImage(pic, this.x, this.y);      
     }
 
-    this.clRect = function() // Метод стирающий прямоугольник
-    {
-        context.clearRect(this.x, this.y, this.width, this.height);
-    }
 }
 
+var image_check = 0;
+//общее кол-во картинок
+var image_need = 18;
 
+//фоновая картинка
+var backgr_pic = new Image();
+backgr_pic.src = 'img/bg.png';
 
-init();
+backgr_pic.onload = function(){
+	image_check++;
+		if(image_check == image_need){
+			init();
+		}
+}
+//голова
+var head_top_pic = new Image();
+head_top_pic.src = 'img/head_top.png';
+
+var head_left_pic = new Image();
+head_left_pic.src = 'img/head_left.png';
+
+var head_right_pic = new Image();
+head_right_pic.src = 'img/head_right.png';
+
+var head_bot_pic = new Image();
+head_bot_pic.src = 'img/head_bot.png';
+
+head_top_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+head_left_pic.onload = function(){
+	image_check++;
+		if(image_check == image_need){
+			init();
+		}
+}
+
+head_right_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+head_bot_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+//5
+//хвост
+
+//тело
+var tail_left_vert_pic = new Image();
+tail_left_vert_pic.src = 'img/tail_left_vert.png';
+
+var tail_right_vert_pic = new Image();
+tail_right_vert_pic.src = 'img/tail_right_vert.png';
+
+var tail_left_gor_pic = new Image();
+tail_left_gor_pic.src = 'img/tail_left_gor.png';
+
+var tail_right_gor_pic = new Image();
+tail_right_gor_pic.src = 'img/tail_right_gor.png';
+
+//изгибы
+var tail_1_pic = new Image();
+tail_1_pic.src = 'img/1.png';
+
+var tail_2_pic = new Image();
+tail_2_pic.src = 'img/2.png';
+
+var tail_3_pic = new Image();
+tail_3_pic.src = 'img/3.png';
+
+var tail_4_pic = new Image();
+tail_4_pic.src = 'img/4.png';
+
+//хвост
+var tail_end_top_pic = new Image();
+tail_end_top_pic.src = 'img/tail_end_top.png';
+
+var tail_end_right_pic = new Image();
+tail_end_right_pic.src = 'img/tail_end_right.png';
+
+var tail_end_bot_pic = new Image();
+tail_end_bot_pic.src = 'img/tail_end_bot.png';
+
+var tail_end_left_pic = new Image();
+tail_end_left_pic.src = 'img/tail_end_left.png';
+
+tail_right_vert_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_left_vert_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_right_gor_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_left_gor_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_1_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_2_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_3_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_4_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_end_top_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_end_right_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_end_bot_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+tail_end_left_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
+//еда
+var food_pic = new Image();
+food_pic.src = 'img/food.png';
+
+food_pic.onload = function(){
+	image_check++;
+	if(image_check == image_need){
+			init();
+		}
+}
+
--- js/tail.js
+++ js/tail.js
@@ -1,7 +1,8 @@
 //первоначалльное создание хвоста
-function createTail(color,quan){
+function createTail(quan){
 	for(var i = 0; i < quan; i++){
-		tail[i] = new rect(color, head.x - side_head * (i+1), head.y, side_head, side_head);
+		//tail[i] = new rect(color, head.x - side_head * (i+1), head.y, side_head, side_head);
+		tail[i] = new imag(head.x - side_head * (i+1), head.y);
 	}
 }
 
@@ -20,19 +21,47 @@ function updateTail(){
 		switch (check) {
 			case 0:	
 					tail[0].x = head.x + side_head;
-					tail[0].y = head.y;		
+					tail[0].y = head.y;
+					// if(tail_side){
+					// 	tail[0].drawImg(tail_left_gor_pic);
+					// 	tail_side = false;}
+					// else{
+					// 	tail[0].drawImg(tail_right_gor_pic);
+					// 	tail_side = true;
+					// }				
 				break;
 			case 1:	
 					tail[0].y = head.y + side_head;
 					tail[0].x = head.x;		
+					// if(tail_side){
+					// 	tail[0].drawImg(tail_left_vert_pic);
+					// 	tail_side = false;}
+					// else{
+					// 	tail[0].drawImg(tail_right_vert_pic);
+					// 	tail_side = true;
+					// }	
 				break;
 			case 2:
 					tail[0].x = head.x - side_head;
-					tail[0].y = head.y;		
+					tail[0].y = head.y;
+					// if(tail_side){
+					// 	tail[0].drawImg(tail_left_gor_pic);
+					// 	tail_side = false;}
+					// else{
+					// 	tail[0].drawImg(tail_right_gor_pic);
+					// 	tail_side = true;
+					// }			
 				break;
 			case 3:
 					tail[0].y = head.y - side_head;
 					tail[0].x = head.x;
+					// if(tail_side){
+					// 	tail[0].drawImg(tail_left_vert_pic);
+					// 	tail_side = false;}
+					// else{
+					// 	tail[0].drawImg(tail_right_vert_pic);
+					// 	tail_side = true;
+					// }	
 				break;
 			default:
 				break;
@@ -46,12 +75,117 @@ function updateTail(){
 }
 
 //отрисовка хвоста
+// function drawTail(){
+
+// 		switch (check) {
+// 			case 0:	
+// 					for(var i = 0; i < tail.length; i++)
+// 						tail[i].drawImg(tail_left_gor_pic);
+// 				break;
+// 			case 1:	
+// 					for(var i = 0; i < tail.length; i++)
+// 						tail[i].drawImg(tail_left_vert_pic);
+// 				break;
+// 			case 2:
+// 					for(var i = 0; i < tail.length; i++)
+// 						tail[i].drawImg(tail_left_gor_pic);
+// 				break;
+// 			case 3:
+// 					for(var i = 0; i < tail.length; i++)
+// 						tail[i].drawImg(tail_left_vert_pic);
+// 				break;
+// 			default:
+// 				break;
+// 		}		
+// }
+
+//отрисовка хвоста
 function drawTail(){
+	tail[-1] = head;
 	for(var i = 0; i < tail.length; i++){
-		tail[i].drawRect();
+
+		//хвост
+		if(i == tail.length - 1){
+			if(tail[i].x > tail[i-1].x)
+			tail[i].drawImg(tail_end_left_pic);
+			
+			if(tail[i].x < tail[i-1].x)
+			tail[i].drawImg(tail_end_right_pic);
+
+			if(tail[i].y > tail[i-1].y)
+			tail[i].drawImg(tail_end_top_pic);
+			
+			if(tail[i].y < tail[i-1].y)
+			tail[i].drawImg(tail_end_bot_pic);
+			
+			break;
+		}
+
+		//горизонтально
+		if((tail[i].y == tail[i-1].y) && (tail[i].y == tail[i+1].y))
+			tail[i].drawImg(tail_left_gor_pic);
+		//вертикально
+		if((tail[i].x == tail[i-1].x) && (tail[i].x == tail[i+1].x))
+			tail[i].drawImg(tail_left_vert_pic);
+
+		//слева-направо сверху-вниз---------------
+		if((tail[i].x == tail[i-1].x) && (tail[i].x > tail[i+1].x) &&(tail[i].y < tail[i-1].y))
+			tail[i].drawImg(tail_1_pic);
+
+		//справа-налево снизу-вверх
+		if((tail[i].x > tail[i-1].x) && (tail[i].x == tail[i+1].x) && (tail[i].y < tail[i+1].y))
+			tail[i].drawImg(tail_1_pic);
+		
+		//слева-направо снизу-вверх----------------
+		if((tail[i].x == tail[i-1].x) && (tail[i].x > tail[i+1].x) &&(tail[i].y > tail[i-1].y))
+			tail[i].drawImg(tail_2_pic);
+
+		//справа-налево сверху-вниз
+		if((tail[i].x > tail[i-1].x) && (tail[i].x == tail[i+1].x) && (tail[i].y > tail[i+1].y))
+			tail[i].drawImg(tail_2_pic);
+
+		//слева-направо сверху-вниз----------------
+		if((tail[i].x < tail[i-1].x) && (tail[i].x == tail[i+1].x) &&(tail[i].y > tail[i+1].y))
+			tail[i].drawImg(tail_3_pic);
+
+		//справа-налево снизу-вверх
+		if((tail[i].x < tail[i+1].x) && (tail[i].x == tail[i-1].x) && (tail[i].y > tail[i-1].y))
+			tail[i].drawImg(tail_3_pic);
+
+		//справа-налево сверху-вниз----------------
+		if((tail[i].x == tail[i-1].x) && (tail[i].x < tail[i+1].x) &&(tail[i].y < tail[i-1].y))
+			tail[i].drawImg(tail_4_pic);
+
+		//слева-направо сверху-вниз
+		if((tail[i].x < tail[i-1].x) && (tail[i].x == tail[i+1].x) &&(tail[i].y < tail[i+1].y))
+			tail[i].drawImg(tail_4_pic);
+	
 	}
+
 }
 
 function addTailItem(){
-	tail.push(new rect('#888', tail[tail.length-1].x + 20, tail[tail.length-1].y + 20, side_head, side_head));
+	tail.push(
+		// new rect('#888', tail[tail.length-1].x + 20, tail[tail.length-1].y + 20, side_head, side_head)
+		 new imag(tail[tail.length-1].x + 20, tail[tail.length-1].y + 20)
+		);
 }
+
+// //функция-коструктор для прямоугольников
+// function rect(color, x, y, width, height) {
+//     this.color = color; // цвет прямоугольника
+//     this.x = x; // координата х
+//     this.y = y; // координата у
+//     this.width = width; // ширина
+//     this.height = height; // высота
+//     this.drawRect = function() // Метод рисующий прямоугольник
+//     {
+//         context.fillStyle = this.color;
+//         context.fillRect(this.x, this.y, this.width, this.height);
+//     }
+
+//     this.clRect = function() // Метод стирающий прямоугольник
+//     {
+//         context.clearRect(this.x, this.y, this.width, this.height);
+//     }
+// }
